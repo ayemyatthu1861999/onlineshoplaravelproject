c59614f56d2a4b1ba55c691fa9386d25a412f11e
aye1861999 <wynedaung8@gmail.com>
2020-09-02 02:06:20 -0800

    welcome blade change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel git update
                 </div>
 
                 <div class="links">
